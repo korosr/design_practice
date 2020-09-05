daaf382e4dc8f69efca358945e290d629d3d9233
korosr <kaoriisa1206@gmail.com>
2020-09-05 10:26:01 +0900

    first commit

---
+++ JIN/js/myscript.js
@@ -0,0 +1,18 @@
+$(function(){
+    load_effect();
+    $(window).scroll(function (){
+        scroll_effect();
+    });
+});
+
+//ふわっとロード(menu以外)
+function load_effect(){
+    var tt = $(window).scrollTop();
+    var hh = $(window).height();
+    $('.fadein').each(function(){
+        var yy = $(this).offset().top;
+        if (tt > yy - hh){
+            $(this).addClass('done');
+        }
+    });
+}
